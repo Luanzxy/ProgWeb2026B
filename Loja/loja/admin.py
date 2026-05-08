d7719df8524e67a903358808b38d90b94a2ac94d
José Luan <luan.ribeiro@escolar.ifrn.edu.br>
2026-05-08 09:54:44 -0300

    Projeto inicial

---
+++ Loja/loja/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+# Register your models here.
+
+from .models import *
+
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao',
+'msgPromocao', 'preco', 'categoria',)
+    
+
+from .models import * #imporata nossos models
+admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do adm
+admin.site.register(Categoria)
+admin.site.register(Produto, ProdutoAdmin)
